07ae0d70abcb29f5a097b4771b58e450aa9be990
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-11-17 05:38:20 -0600

    Update matpower reader

---
+++ psst/matpower/__init__.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+"""
+MATPOWER module in `psst`
+Copyright (C) 2016 Dheepak Krishnamurthy
+"""
+
+
+import os
+
+import pandas as pd
+
+from .case_reader import parse_file
+from .columns import COLUMNS, ATTRIBUTES
+
+
+class MPC(object):
+    _attributes = ATTRIBUTES
+
+    def __init__(self, filename, mode='r'):
+        self._filename = filename
+        if mode == 'r' or mode == 'read':
+            read_matpower(self)
+
+
+class MPCDataFrame(object):
+    _attributes = ATTRIBUTES
+
+
+def to_dataframe(mpc):
+    mpc_df = MPCDataFrame()
+    for attribute in mpc_df._attributes:
+        array = getattr(mpc, attribute, None)
+        if array.size == 1:
+            setattr(mpc_df, attribute, array.tostring())
+        else:
+            columns = COLUMNS.get(attribute, [i for i in range(0, array.shape[1])])
+            columns = columns[:array.shape[1]]
+            if array.shape[1] > len(columns):
+                columns = columns[:-1] + ['{}_{}'.format(columns[-1], i) for i in range(0, array.shape[1] - len(columns) + 1)]
+            df = pd.DataFrame(array, columns=columns)
+            setattr(mpc_df, attribute, df)
+    return mpc_df
+
+
+def read_matpower(mpc):
+
+    if isinstance(mpc, MPC):
+        filename = mpc._filename
+    else:
+        filename = mpc
+        mpc = MPC(filename)
+
+    with open(os.path.abspath(mpc._filename)) as f:
+        string = f.read()
+    string = string + '\n\n'
+
+    for attribute in mpc._attributes:
+        array = parse_file(attribute, string)
+        if array is not None:
+            setattr(mpc, attribute, array)
+
+    return mpc
+
+
+def write_matpower(mpc):
+    NotImplementedError("write_matpower is not implemented yet")
+
+
--- psst/matpower/case_reader.py
+++ psst/matpower/case_reader.py
@@ -8,73 +8,37 @@ Copyright (C) 2016 Dheepak Krishnamurthy
 
 
 import os
+import re
 from builtins import open
+
 import numpy as np
-import pandas as pd
 
 
-class MPC(object):
-    _attributes = ['version', 'baseMVA', 'areas', 'bus', 'gen', 'gencost', 'branch']
+def parse_file(attribute, string):
 
-    def __init__(self, filename):
+    match = search_file(attribute, string)
 
-        self._filename = filename
+    if match is not None:
+        match = match.strip("'").strip('"')
 
-        with open(os.path.abspath(self._filename)) as f:
-            self._original_mpc = f.read()
+        _list = list()
+        for line in match.splitlines():
+            if line.strip():
+                _list.append(line.strip().strip(';').strip().split())
 
-        self._mpc = []
-        for line in self._original_mpc.splitlines():
-            self._mpc.append(line)
+        array = np.array(_list)
+        return array
+    else:
+        return match
 
-        self._mpc = '\n'.join(self._mpc)
-        self._parse_file()
 
-        self.gen['name'] = ["GenCo{}".format(i) for i in range(0, self.gen.shape[0])]
-        self.gen.set_index('name', inplace=True)
+def search_file(attribute, string):
+    pattern = 'mpc\.{}\s*=\s*[\[]?[\n]?(?P<data>.*?)[\n]?[\]]?;\n\n'.format(attribute)
 
-        self.gencost['name'] = ["GenCo{}".format(i) for i in range(0, self.gen.shape[0])]
-        self.gencost.set_index('name', inplace=True)
+    match = re.search(pattern, string, re.DOTALL )
+    if match is not None:
+        return match.groupdict().get('data', None)
+    else:
+        return match
 
-        self.gen = pd.concat([self.gen, self.gencost], axis=1)
 
-    def _parse_file(self):
-
-        position = {}
-
-        for linenumber, line in enumerate(self._mpc.splitlines()):
-            attribute = line.split('=')[0]
-            if attribute == line or line.startswith('%'):
-                continue
-            else:
-                attribute = attribute.strip().replace('mpc.', '')
-            if 'function' in attribute:
-                continue
-            if attribute:
-                position[attribute] = linenumber
-
-        for attribute in position.keys():
-
-            linenumber = position[attribute]
-            columns = self._mpc.splitlines()[linenumber - 1].strip('%').strip().split('%')[0].strip().split('\t')
-
-            line = self._mpc.splitlines()[linenumber]
-            if '[' not in line:
-                array = float(line.split('=')[1].strip(';').replace("'", '').strip())
-                setattr(self, attribute, np.array(array))
-            else:
-                array = []
-                for i, line in enumerate(self._mpc.splitlines()[linenumber:]):
-                    if '[' in line:
-                        continue
-                    if ']' in line:
-                        break
-                    line = line.split('%')[0]
-                    row = [float(item.strip().replace(';', '')) for item in line.strip(';').strip().split('\t')]
-
-                    array.append(row)
-
-                if attribute=='gencost':
-                    columns=['cost_model', 'startup_cost', 'shutdown_cost', 'N'] + ['C{}'.format(i - 1) for i in range(np.array(array).shape[1] - 4, 0, -1)]
-                setattr(self, '_columns_{}'.format(attribute), columns)
-                setattr(self, attribute, pd.DataFrame(np.array(array), columns=columns))
