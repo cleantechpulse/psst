8813f7e305bbd76ff21248a0a76f0adda28770f3
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2016-11-20 13:55:15 -0700

    Add read_matpower

--- psst/case/matpower/__init__.py
+++ psst/case/matpower/__init__.py
@@ -4,57 +4,60 @@
 MATPOWER module in `psst`
 Copyright (C) 2016 Dheepak Krishnamurthy
 """
+from __future__ import print_function, absolute_import
 
-
+import re
+import logging
 import os
 
 import pandas as pd
 
-from .case_reader import parse_file, find_attributes
+from .reader import parse_file, find_attributes
 from .utils import COLUMNS
+from ...utils import int_else_float_except_string
+
+logging.basicConfig()
+logger = logging.getLogger(__file__)
 
 
-class MPCDataFrame(object):
+class MPC(object):
     def __init__(self, filename=None, mode='r'):
-        self.ATTRIBUTES = []
+        self._attributes = []
         if filename is not None:
             self._filename = filename
             if mode == 'r' or mode == 'read':
-                read_matpower(self)
+                self._read_matpower(self)
+
+    @classmethod
+    def _read_matpower(cls, mpc):
+
+        if not isinstance(mpc, cls):
+            filename = mpc
+            mpc = cls(filename, mode=None)
+
+        with open(os.path.abspath(mpc._filename)) as f:
+            string = f.read()
+
+        for attribute in find_attributes(string):
+            _list = parse_file(attribute, string)
+            if _list is not None:
+                if len(_list) == 1:
+                    setattr(mpc, attribute, _list[0][0])
+                else:
+                    cols = max([len(l) for l in _list])
+                    columns = COLUMNS.get(attribute, [i for i in range(0, cols)])
+                    columns = columns[:cols]
+                    if cols > len(columns):
+                        columns = columns[:-1] + ['{}_{}'.format(columns[-1], i) for i in range(0, cols - len(columns) + 1)]
+                    df = pd.DataFrame(_list, columns=columns)
+                    setattr(mpc, attribute, df)
+                mpc._attributes.append(attribute)
+
+        return mpc
+
+    def _write_matpower(cls, filename, mpc=None):
+        NotImplementedError('MATPOWER case writer is not implemented. Please contact the developer')
 
 
-def read_matpower(mpc):
-
-    if not isinstance(mpc, MPCDataFrame):
-        filename = mpc
-        mpc = MPCDataFrame(filename)
-
-    with open(os.path.abspath(mpc._filename)) as f:
-        string = f.read()
-
-    for attribute in find_attributes(string):
-        _list = parse_file(attribute, string)
-        if _list is not None:
-            if len(_list) == 1:
-                setattr(mpc, attribute, _list[0])
-            else:
-                cols = _max_cols(_list)
-                columns = COLUMNS.get(attribute, [i for i in range(0, cols)])
-                columns = columns[:cols]
-                if cols > len(columns):
-                    columns = columns[:-1] + ['{}_{}'.format(columns[-1], i) for i in range(0, cols - len(columns) + 1)]
-                df = pd.DataFrame(_list, columns=columns)
-                setattr(mpc, attribute, df)
-            mpc.ATTRIBUTES.append(attribute)
-
-    return mpc
-
-
-def _max_cols(_list):
-    return max([len(l) for l in _list])
-
-
-def write_matpower(mpc):
-    NotImplementedError("write_matpower is not implemented yet")
-
-
+read_matpower = MPC._read_matpower
+write_matpower = MPC._write_matpower
